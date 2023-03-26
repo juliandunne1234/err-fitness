3991e0b8a7b8f66aaf52ad5d3d82372c97c98913
juliandunne1234 <julianmorandunne@gmail.com>
2023-03-26 14:06:12 +0000

    added products views and templates

--- products/views.py
+++ products/views.py
@@ -1,1 +1,13 @@
 from django.shortcuts import render
+from .models import Product
+
+
+def all_products(request):
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
