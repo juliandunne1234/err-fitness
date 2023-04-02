8e441e7d5d2711adb4b06bc242fba9b65933e1e3
juliandunne1234 <julianmorandunne@gmail.com>
2023-04-02 07:47:24 +0000

    added bag context and delivery logic

---
+++ bag/contexts.py
@@ -0,0 +1,18 @@
+
+
+def bag_contents(request):
+
+    bag_items = []
+    total_cost = 0
+    product_count = 0
+
+    if total > 0:
+        total_cost += settings.STANDARD_DELIVERY_CHARGE
+
+    context = {
+        'bag_items':bag_items,
+        'total_cost':total_cost,
+        'product_count':product_count,
+    }
+
+    return context
