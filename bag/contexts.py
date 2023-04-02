6407009970254c563cce95d568dd92a16d8e3ded
juliandunne1234 <julianmorandunne@gmail.com>
2023-04-02 09:07:21 +0000

    added add_to_bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -6,13 +6,13 @@ def bag_contents(request):
     total_cost = 0
     product_count = 0
 
-    if total > 0:
+    if total_cost > 0:
         total_cost += settings.STANDARD_DELIVERY_CHARGE
 
     context = {
-        'bag_items':bag_items,
-        'total_cost':total_cost,
-        'product_count':product_count,
+        'bag_items': bag_items,
+        'total_cost': total_cost,
+        'product_count': product_count,
     }
 
     return context
