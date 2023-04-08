8b8093b06838ce69d135ae7254aa2bc6cec97ff6
juliandunne1234 <julianmorandunne@gmail.com>
2023-04-08 14:11:00 +0000

    added basic checkout functionality

--- err_fitness/settings.py
+++ err_fitness/settings.py
@@ -173,3 +173,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STANDARD_DELIVERY_CHARGE = 10
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
