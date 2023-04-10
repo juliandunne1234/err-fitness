dcde3b1301985c3c3f943c9f20b614bc2b6e8181
juliandunne1234 <julianmorandunne@gmail.com>
2023-04-10 15:14:22 +0000

    added workout_plan template and view logic

--- workout_plan/views.py
+++ workout_plan/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import WorkoutPlan
 
 
 def workout_plan(request):
-    return render(request, 'workout_plan/workout_plan.html')
+
+    workout_plan_daily = WorkoutPlan.objects.all()
+
+    context = {
+        'workout_plan_daily': workout_plan_daily,
+    }
+
+    return render(request, 'workout_plan/workout_plan.html', context)
