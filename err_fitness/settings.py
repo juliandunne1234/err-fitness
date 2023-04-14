0df6fa3fcb3a1154e20b65d6dea84202603a0a76
juliandunne1234 <julianmorandunne@gmail.com>
2023-04-14 16:51:39 +0000

    completed confirmation emails

--- err_fitness/settings.py
+++ err_fitness/settings.py
@@ -179,3 +179,4 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET_ERR', '')
+DEFAULT_FROM_EMAIL = 'errfitness@example.com'
